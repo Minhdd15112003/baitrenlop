28943238be55bab693490552227c62c57bd456be
Minhdd15112003 <139239663+Minhdd15112003@users.noreply.github.com>
2024-04-12 01:01:05 +0700

    Update socket-io.js

--- mene-base/socket/socket-io.js
+++ mene-base/socket/socket-io.js
@@ -5,11 +5,6 @@ const socketAPI = {
 }
 objIO.on("connection", (socket) => {
     console.log("Kết nối với client thành công!");
-
-    // Handle disconnection
-    socket.on("disconnect", () => {
-        console.log("Kết nối với client thất bại!");
-    });
 });
 
 module.exports = socketAPI;
